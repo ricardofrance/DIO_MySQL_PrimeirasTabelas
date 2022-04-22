ee3a1d8c996794f6a1d8b3159eb141f03287dbec
Ricardo Leme Francé <ricardolfrance@gmail.com>
2022-04-22 12:16:43 -0300

    Primeiras Tabelas

--- dados.sql
+++ dados.sql
@@ -6,3 +6,20 @@ CREATE TABLE pessoas (
 
 INSERT INTO pessoas (nome,nascimento) VALUES ('Ricardo Francé', '1978-08-30')
 INSERT INTO pessoas (nome,nascimento) VALUES ('Otávio Francé', '2002-08-16')
+
+
+ALTER TABLE `pessoa` ADD `genero` VARCHAR(20) NOT NULL AFTER `nascimento`;
+
+UPDATE pessoa SET genero='F' WHERE id=1
+
+SELECT * FROM pessoas
+
+UPDATE pessoas SET nome = ‘Ricardo’
+
+UPDATE pessoas SET nome = ‘Ricardo’ WHERE id=1;
+
+DELETE FROM pessoas WHERE id=1
+
+SELECT * FROM pessoas ORDER BY nome
+
+SELECT COUNT(qtd), GENERO FROM pessoas GROUP BY genero
